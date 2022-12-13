776ff6f171f380a134acf6ba37ff6c1b0734ea2d
Abdul Hannan <abdulhannan.002buic@gmail.com>
2022-12-13 15:34:01 +0500

    First Commit

---
+++ routes/users.js
@@ -0,0 +1,35 @@
+var express = require('express');
+var router = express.Router();
+var usersController = require('../controllers/usersController');
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  res.send('respond with a resource');
+});
+
+
+router.get('/usersList', async function(req, res, next){
+  const query = req.query;
+  console.log(query);
+  try{
+    const result = await usersController.getAllUsers(query);
+    res.status(200).send(result);
+  }
+  catch(error){
+    res.status(500).send(error);
+  }
+});
+
+router.post('/adduser', async function(req, res, next){
+  const body = req.body;
+  console.log(body);
+  try{
+    const result = await usersController.addUser(body);
+    res.status(200).send(result);
+  }
+  catch(error){
+    res.status(500).send(error);
+  }
+});
+
+module.exports = router;
